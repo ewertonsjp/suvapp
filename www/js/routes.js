57b93d715ddc052dd8a6890a1e12042604141845
Ewerton Costa <ewertonsjp@gmail.com>
2017-01-23 21:30:15 -0300

    Adiciona arquivos criados pelo Ionic Creator

---
+++ www/js/routes.js
@@ -0,0 +1,25 @@
+angular.module('app.routes', []).config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+    .state('family', {
+      url: '/family',
+      templateUrl: 'templates/family.html',
+      controller: 'familyCtrl'
+    })
+
+    .state('invoice', {
+      url: '/invoice',
+      templateUrl: 'templates/invoice.html',
+      controller: 'invoiceCtrl'
+    })
+
+    .state('transaction', {
+      url: '/transaction/create',
+      templateUrl: 'templates/transaction.html',
+      controller: 'transactionCtrl'
+    })
+
+    $urlRouterProvider.otherwise('/family')
+
+});
